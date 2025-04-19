17fafb66f95a8fb04ea5e727537cf23a0072163f
HTony03 <HTony03@foxmail.com>
2025-04-19 10:37:22 +0800

    changes

--- nonebot_plugin_github_release_notifier/__init__.py
+++ nonebot_plugin_github_release_notifier/__init__.py
@@ -21,7 +21,7 @@ from .db_action import (
 from .commands import repo_group
 from .config import Config
 
-__version__ = "0.1.8"
+__version__ = "0.1.8.1"
 cmd_group = repo_group
 
 __plugin_meta__ = PluginMetadata(
