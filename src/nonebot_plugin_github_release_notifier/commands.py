4c4ed75ca06420768cd038e90814dfac459f4a2c
HTony03 <HTony03@foxmail.com>
2025-06-16 17:03:48 +0800

    feat: markdown render for repo.info

--- src/nonebot_plugin_github_release_notifier/commands.py
+++ src/nonebot_plugin_github_release_notifier/commands.py
@@ -397,10 +397,8 @@ Last updated: {updated}''' + \
                           ('' if allow_fork else 'The repo does not allow forks\n') + \
                           ('The repo is a fork\n' if is_fork else '') + \
                           ('The repo is archived\n' if is_archived else '')
-
-                re_text = message.replace('\n', '<br />')
                 message = MessageSegment.image(
-                    await html_to_pic(f'<p>{re_text}</p>'))
+                    await md_to_pic(message))
     except aiohttp.ClientResponseError as e:
         message = (
             f"Failed to fetch GitHub repo usage: {e.status} - {e.message}"
