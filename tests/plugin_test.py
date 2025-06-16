bd65c6fc9296e4da5f993d37aba3eacc64781d8f
HTony03 <HTony03@foxmail.com>
2025-06-16 20:55:27 +0800

    fix: pytest (commmit -> commit)

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -53,10 +53,76 @@ async def test_add_repo(app: App) -> None:
         adapter = nonebot.get_adapter(OnebotV11Adapter)
         bot = ctx.create_bot(base=Bot, adapter=adapter)
         ctx.receive_event(bot, event1)
-        ctx.should_call_send(event1, ANY(), result=None, bot=bot)
-        ctx.should_finished()
+        ctx.should_call_send(event1, ANY())
         ctx.receive_event(bot, event2)
-        ctx.should_call_send(event2, ANY(), result=None, bot=bot)
+        ctx.should_call_send(event2, ANY())
+        ctx.should_finished()
+
+
+@pytest.mark.asyncio
+async def test_repo_info(app: App):
+    import nonebot
+    from nonebot import require
+    from nonebot.adapters.onebot.v11 import Adapter as OnebotV11Adapter
+
+    assert require("nonebot_plugin_github_release_notifier")
+
+    # 构造 shell command 事件
+    from nonebot.adapters.onebot.v11.event import MessageEvent
+    from nonebot.adapters.onebot.v11 import Message
+
+    event1 = MessageEvent(
+        time=123456,
+        self_id=123456,
+        post_type="message",
+        user_id=1234567890,
+        message_type="group",
+        message_id=1234567890,
+        message=Message("/repo.info HTony03/nonebot_plugin_github_release_notifier"),
+        raw_message="/repo.info HTony03/nonebot_plugin_github_release_notifier",
+        original_message=Message("/repo.info HTony03/nonebot_plugin_github_release_notifier"),
+        sub_type="normal",
+        sender={"user_id": 1234567890, "nickname": "测试用户"},
+        font=123,
+    )
+    event2 = MessageEvent(
+        time=123456,
+        self_id=123456,
+        post_type="message",
+        user_id=1234567890,
+        message_type="group",
+        message_id=1234567890,
+        message=Message("/latest_commit HTony03/nonebot_plugin_github_release_notifier"),
+        raw_message="/latest_commit HTony03/nonebot_plugin_github_release_notifier",
+        original_message=Message("/latest_commit HTony03/nonebot_plugin_github_release_notifier"),
+        sub_type="normal",
+        sender={"user_id": 1234567890, "nickname": "测试用户"},
+        font=123,
+    )
+    event3 = MessageEvent(
+        time=123456,
+        self_id=123456,
+        post_type="message",
+        user_id=1234567890,
+        message_type="group",
+        message_id=1234567890,
+        message=Message("/latest_release HTony03/nonebot_plugin_github_release_notifier"),
+        raw_message="/latest_release HTony03/nonebot_plugin_github_release_notifier",
+        original_message=Message("/latest_release HTony03/nonebot_plugin_github_release_notifier"),
+        sub_type="normal",
+        sender={"user_id": 1234567890, "nickname": "测试用户"},
+        font=123,
+    )
+
+    async with app.test_matcher() as ctx:
+        adapter = nonebot.get_adapter(OnebotV11Adapter)
+        bot = ctx.create_bot(base=Bot, adapter=adapter)
+        ctx.receive_event(bot, event1)
+        ctx.should_call_send(event1, ANY())
+        ctx.receive_event(bot, event2)
+        ctx.should_call_send(event2, ANY())
+        ctx.receive_event(bot, event3)
+        ctx.should_call_send(event3, ANY())
         ctx.should_finished()
 
 
@@ -94,71 +160,3 @@ async def test_check_usage(app: App):
         ctx.should_call_send(event1, ANY(), result=None, bot=bot)
         ctx.should_finished()
 
-
-@pytest.mark.asyncio
-async def test_repo_info(app: App):
-    import nonebot
-    from nonebot import require
-    from nonebot.adapters.onebot.v11 import Adapter as OnebotV11Adapter
-
-    assert require("nonebot_plugin_github_release_notifier")
-
-    # 构造 shell command 事件
-    from nonebot.adapters.onebot.v11.event import MessageEvent
-    from nonebot.adapters.onebot.v11 import Message
-
-    event1 = MessageEvent(
-        time=123456,
-        self_id=123456,
-        post_type="message",
-        user_id=1234567890,
-        message_type="group",
-        message_id=1234567890,
-        message=Message("/repo.info HTony03/nonebot_plugin_github_release_notifier"),
-        raw_message="/repo.info HTony03/nonebot_plugin_github_release_notifier",
-        original_message=Message("/repo.info HTony03/nonebot_plugin_github_release_notifier"),
-        sub_type="normal",
-        sender={"user_id": 1234567890, "nickname": "测试用户"},
-        font=123,
-    )
-    event2 = MessageEvent(
-        time=123456,
-        self_id=123456,
-        post_type="message",
-        user_id=1234567890,
-        message_type="group",
-        message_id=1234567890,
-        message=Message("/latest_commmit HTony03/nonebot_plugin_github_release_notifier"),
-        raw_message="/latest_commmit HTony03/nonebot_plugin_github_release_notifier",
-        original_message=Message("/latest_commmit HTony03/nonebot_plugin_github_release_notifier"),
-        sub_type="normal",
-        sender={"user_id": 1234567890, "nickname": "测试用户"},
-        font=123,
-    )
-    event3 = MessageEvent(
-        time=123456,
-        self_id=123456,
-        post_type="message",
-        user_id=1234567890,
-        message_type="group",
-        message_id=1234567890,
-        message=Message("/latest_release HTony03/nonebot_plugin_github_release_notifier"),
-        raw_message="/latest_release HTony03/nonebot_plugin_github_release_notifier",
-        original_message=Message("/latest_release HTony03/nonebot_plugin_github_release_notifier"),
-        sub_type="normal",
-        sender={"user_id": 1234567890, "nickname": "测试用户"},
-        font=123,
-    )
-
-    async with app.test_matcher() as ctx:
-        adapter = nonebot.get_adapter(OnebotV11Adapter)
-        bot = ctx.create_bot(base=Bot, adapter=adapter)
-        ctx.receive_event(bot, event1)
-        ctx.should_call_send(event1, ANY(), result=None, bot=bot)
-        ctx.should_finished()
-        ctx.receive_event(bot, event2)
-        ctx.should_call_send(event2, ANY(), result=None, bot=bot)
-        ctx.should_finished()
-        ctx.receive_event(bot, event3)
-        ctx.should_call_send(event3, ANY(), result=None, bot=bot)
-        ctx.should_finished()
