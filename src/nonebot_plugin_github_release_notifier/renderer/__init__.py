4a0388a8afc24cc47b08d4178097c748ef8d49f2
HTony03 <HTony03@foxmail.com>
2025-08-13 17:08:22 +0800

    feat: github issue commit update
    refactor: repo_activity
    changes:
    from aiohttp to githubkit responses
    config: template -> premade language files

---
+++ src/nonebot_plugin_github_release_notifier/renderer/__init__.py
@@ -0,0 +1,128 @@
+"""
+@Author         : yanyongyu
+@Date           : 2021-03-09 16:45:25
+@LastEditors    : yanyongyu
+@LastEditTime   : 2024-06-10 12:15:55
+@Description    : GitHub image renderer
+@GitHub         : https://github.com/yanyongyu
+"""
+
+__author__ = "yanyongyu"
+
+from datetime import date
+from hashlib import sha256
+
+from pydantic_core import to_json
+from githubkit.versions.latest import models
+from playwright.async_api import Route, Request
+from nonebot.adapters.github import OAuthBot, GitHubBot
+
+from src.plugins.github import config
+from src.providers.playwright import get_new_page
+# from src.plugins.github.cache.rendered_image import (
+#     get_rendered_image,
+#     save_rendered_image,
+# )
+
+from .context import (
+    IssueContext,
+    ReadmeContext,
+    IssueClosedContext,
+    IssueOpenedContext,
+    IssueCommentedContext,
+)
+from .render import (
+    issue_to_html,
+    readme_to_html,
+    issue_closed_to_html,
+    issue_opened_to_html,
+    issue_commented_to_html,
+)
+
+WIDTH = 800
+HEIGHT = 30
+
+
+async def _github_html_to_image(html: str, context_url: str | None = None) -> bytes:
+    async def _intercept_network_request(route: Route, request: Request):
+        if context_url and request.url == context_url:
+            await route.fulfill(status=200, body=html, content_type="text/html")
+            return
+        await route.continue_()
+
+    async with get_new_page(viewport={"width": WIDTH, "height": HEIGHT}) as page:
+        if context_url:
+            await page.route(lambda url: url == context_url, _intercept_network_request)
+            await page.goto(context_url)
+        else:
+            await page.set_content(html)
+
+        # expand all details
+        await page.evaluate(
+            "() => document.querySelectorAll('details').forEach(e => e.open = true)"
+        )
+        return await page.screenshot(timeout=60_000, full_page=True)
+
+
+def _context_hash(
+        context: (
+                ReadmeContext
+                | IssueContext
+                | IssueOpenedContext
+                | IssueCommentedContext
+                | IssueClosedContext
+        ),
+) -> str:
+    context_json = to_json(context)
+    return sha256(context_json).hexdigest()
+
+
+async def readme_to_image(
+    repo: models.FullRepository, readme: str
+) -> bytes:
+    """Render a github issue/pr timeline to image"""
+    context = await ReadmeContext.from_repo_readme(repo, readme)
+    # context_hash = _context_hash(context)
+    # if cached_image := await get_rendered_image("readme", context_hash):
+    #     return cached_image
+
+    html = await readme_to_html(context, theme=config.github_theme)
+    image = await _github_html_to_image(
+        html,
+        f"https://raw.githubusercontent.com/{repo.owner.login}/{repo.name}/{repo.default_branch}/",
+    )
+    # await save_rendered_image("readme", context_hash, image)
+    return image
+
+
+async def issue_to_image(
+    bot: GitHubBot | OAuthBot, issue: models.Issue, highlight_comment: int | None = None
+) -> bytes:
+    """Render a github issue/pr timeline to image"""
+    context = await IssueContext.from_issue(bot, issue, highlight_comment)
+    # context_hash = _context_hash(context)
+    # if cached_image := await get_rendered_image("issue", context_hash):
+    #     return cached_image
+
+    html = await issue_to_html(context, theme=config.github_theme)
+    image = await _github_html_to_image(html)
+    # await save_rendered_image("issue", context_hash, image)
+    return image
+
+
+async def issue_opened_to_image(
+    bot: GitHubBot | OAuthBot,
+    repo: models.RepositoryWebhooks,
+    issue: models.WebhookIssuesOpenedPropIssue | models.PullRequestWebhook,
+) -> bytes:
+    """Render webhook event issue/opened to image"""
+    context = await IssueOpenedContext.from_webhook(bot, repo, issue)
+    # context_hash = _context_hash(context)
+    # if cached_image := await get_rendered_image("issue_opened", context_hash):
+    #     return cached_image
+
+    html = await issue_opened_to_html(context, theme=config.github_theme)
+    image = await _github_html_to_image(html)
+    # await save_rendered_image("issue_opened", context_hash, image)
+    return image
+
